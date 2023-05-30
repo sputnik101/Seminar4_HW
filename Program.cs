cd28099e42f27237b1eba921c329b321d5bc695f
ionizy <ionizy@gmail.com>
2023-05-30 22:28:15 +0300

    add lesson

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿//Задача1
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int GetSumNumber()
+// {
+//     int sum=0;
+//     int count = 1;
+
+//     while (count<=num)
+//     {
+//         sum=sum+count;
+//         count= count+1;
+//     }
+//     return sum;
+// }
+
+// int result = GetSumNumber();
+
+// Console.WriteLine(result);
+
+//Задача2
+
+// Console.WriteLine("Введите число");
+// string A = Console.ReadLine();
+// Console.WriteLine(Length());
+
+// int Length () {
+//     int length = A.Length;
+//     return length;
+// }
+
+//Задача3
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int x()
+// {
+//     int result = 1;
+//     int count = 1;
+//     while (count <= num)
+//     {
+//         result = result * count;
+//         count = count + 1;
+//     }
+//     return result;
+// }
+// Console.WriteLine(x());
+
+//Задача4
+
+// void CreateArray()
+// {
+//     int[] arr = new int[8];
+//     int count = 0;
+//     while(count < arr.Length)
+//     {
+//         arr [count] = new Random().Next(0,2);
+//         Console.Write($"{arr[count]} ");
+//         count++;
+//     }
+    
+// }
+// CreateArray();
+
